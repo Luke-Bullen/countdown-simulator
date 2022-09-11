3135efce285fdcf0aeb34d0fc718b67350fc16fb
Luke Bullen <lukeabullen@gmail.com>
2022-09-11 20:12:58 +0100

    function built and some styling - need to refine styling and add functions at bottom of application.html.erb file

--- Gemfile
+++ Gemfile
@@ -75,3 +75,6 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+# needed for parsing JSON
+gem 'open-uri'
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'games/new'
-  post 'games/score'
-  root to: "pages#home"
+  get 'new', to: 'games#new'
+  post 'score', to: 'games#score'
+  root to: "games#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
